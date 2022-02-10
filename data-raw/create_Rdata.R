e03e9bc931e22f5237ab8bc307176092c5a86d76
Your Github NAme <your_github_email@.com>
2022-02-10 14:17:11 -0300

    add script that create .Rdata

---
+++ data-raw/create_Rdata.R
@@ -0,0 +1,12 @@
+### This code covert shapefiles to Rdata (all data examples of packages in /data directory should be a .Rdata )
+
+
+grid_ID_2_polygon_masked <- raster::shapefile("inst/extdata/grid_id_2/grid_ID_2_polygon_masked.shp")
+usethis::use_data(grid_ID_2_polygon_masked, compress = "bzip2")
+
+ne_10m_admin_0_countries <- raster::shapefile("inst/extdata/0_basemap/ne_10m_admin_0_countries.shp")
+usethis::use_data(ne_10m_admin_0_countries, compress = "bzip2")
+
+
+
+
