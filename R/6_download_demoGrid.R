886a9596bb47e5122868e94840319b0025cbba45
Xiao Feng <shandongfx@users.noreply.github.com>
2022-04-11 10:12:02 -0400

    Add files via upload

--- R/6_download_demoGrid.R
+++ R/6_download_demoGrid.R
@@ -1,16 +1,21 @@
-download_demoGrid = function(){
+download_demoGrid = function(downloadNew=F){
   share_url = "https://zenodo.org/record/6447902/files/2_clean_grid.zip?download=1"
-
+  
   local_path = system.file(package="gridder")
-  local_path = system.file(package="dismo")
+  #local_path = system.file(package="dismo")
   
   zip_path = paste0(local_path,"/data/demoGrid.zip")
-  zip_to_path = paste0(local_path,"/data")
-  dir.create(zip_to_path)
   
-  utils::download.file(url=share_url,mode = "wb",
-                       destfile=zip_path
-  )
-  
-  utils::unzip(zip_path,exdir=zip_to_path) 
+  if(  !file.exists(zip_path)   ){
+    
+    if(downloadNew){
+      zip_to_path = paste0(local_path,"/data")
+      dir.create(zip_to_path)
+      
+      utils::download.file(url=share_url,mode = "wb",
+                           destfile=zip_path
+      )
+      utils::unzip(zip_path,exdir=zip_to_path) 
+    }
+  }
 }
