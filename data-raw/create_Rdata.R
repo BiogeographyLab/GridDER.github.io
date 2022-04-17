3621fdfaa23be9036f1d1a2cc0697f729e502c5d
Tai-Rocha <taina013@gmail.com>
2022-04-17 18:06:15 -0300

    testing the pcgk dataset recreated

--- data-raw/create_Rdata.R
+++ data-raw/create_Rdata.R
@@ -4,11 +4,26 @@
 
 
 grid_ID_2_polygon_masked <- raster::shapefile("inst/extdata/grid_id_2/grid_ID_2_polygon_masked.shp")
-usethis::use_data(grid_ID_2_polygon_masked, compress = "bzip2")
+usethis::use_data(grid_ID_2_polygon_masked)
 
 ne_10m_admin_0_countries <- raster::shapefile("inst/extdata/0_basemap/ne_10m_admin_0_countries.shp")
-usethis::use_data(ne_10m_admin_0_countries, compress = "bzip2")
+usethis::use_data(ne_10m_admin_0_countries)
 
 
+## Grid_id_9 (Flora of Vladimir- Russia)
 
+grid_ID_9 <- sf::st_read("inst/extdata/grid_id_9/grid_ID_9.kml")
+usethis::use_data(grid_ID_9)
+
+## GBIF occurrences of gird_id_9 system
+
+occs_grid_id_9 <- readr::read_csv("inst/extdata/grid_id_9/0060040-210914110416597.csv")
+usethis::use_data(occs_grid_id_9)
+
+
+## CRS List object
+
+crs_list_prj <- readRDS("data/crs_list_prj.rds")
+
+usethis::use_data(crs_list_prj)
 
