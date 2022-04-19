5b3541e5531f35c842a254ee5bdea8a3ebed5707
Tai-Rocha <taina013@gmail.com>
2022-04-19 17:01:36 -0300

    infer_crs remove source

--- R/infer_crs.R
+++ R/infer_crs.R
@@ -29,8 +29,6 @@ infer_crs = function(occ_path,
   library(raster)
   library(rgdal)
 
-  source("inst/extdata/1_source_seek_occ_pattern.R")
-  source("inst/extdata/grid_generation.R")
   catf <- function(..., file="temp/temp.log", append=TRUE){
     cat(..., file=file, append=append)
   }
