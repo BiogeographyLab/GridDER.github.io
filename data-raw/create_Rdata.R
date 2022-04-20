e1fe38cd64a5dfcfb18ddbe3649393005389a6d7
Tai-Rocha <taina013@gmail.com>
2022-04-20 12:42:35 -0300

    updating internal data

--- data-raw/create_Rdata.R
+++ data-raw/create_Rdata.R
@@ -3,27 +3,27 @@
 ## For while I do not put the raw data at repo package (.csv or .shp), once data that will use as examples is provided by the package in .RData format
 
 
-grid_ID_2_polygon_masked <- raster::shapefile("inst/extdata/grid_id_2/grid_ID_2_polygon_masked.shp")
-usethis::use_data(grid_ID_2_polygon_masked)
+grid_ID_2_polygon_masked <- raster::shapefile("inst/extdata/grid_id_2/grid_ID_2_polygon_masked.shp", )
+usethis::use_data(grid_ID_2_polygon_masked, internal = TRUE, overwrite = TRUE)
 
 ne_10m_admin_0_countries <- raster::shapefile("inst/extdata/0_basemap/ne_10m_admin_0_countries.shp")
-usethis::use_data(ne_10m_admin_0_countries)
+usethis::use_data(ne_10m_admin_0_countries, internal = TRUE, overwrite = TRUE)
 
 
 ## Grid_id_9 (Flora of Vladimir- Russia)
 
 grid_ID_9 <- sf::st_read("inst/extdata/grid_id_9/grid_ID_9.kml")
-usethis::use_data(grid_ID_9)
+usethis::use_data(grid_ID_9, internal = TRUE, overwrite = TRUE)
 
 ## GBIF occurrences of gird_id_9 system
 
 occs_grid_id_9 <- readr::read_csv("inst/extdata/grid_id_9/0060040-210914110416597.csv")
-usethis::use_data(occs_grid_id_9)
+usethis::use_data(occs_grid_id_9, internal = TRUE, overwrite = TRUE)
 
 
 ## CRS List object
 
 crs_list_prj <- readRDS("data/crs_list_prj.rds")
 
-usethis::use_data(crs_list_prj)
+usethis::use_data(crs_list_prj, internal = TRUE, overwrite = TRUE)
 
