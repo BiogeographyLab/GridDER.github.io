4a0c06022bb3f2f92f4ca9685241d277e2b121ce
Tai-Rocha <taina013@gmail.com>
2022-05-04 10:25:32 -0300

    grid_generation test: changing line 120

--- R/grid_generation.R
+++ R/grid_generation.R
@@ -117,7 +117,7 @@ grid_generation = function(res_x = 10,
     flag_loadCountryPolygon= TRUE
   }
   if(flag_loadCountryPolygon){
-    country_shp = raster::shapefile("data/0_basemap/ne_10m_admin_0_countries.shp")
+    country_shp = data("data/ne_10m_admin_0_countries.rda")
 
     one_country = subset(country_shp,ADMIN %in% country)
 
