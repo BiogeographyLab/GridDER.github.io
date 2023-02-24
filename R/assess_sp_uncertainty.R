190ae11a58b84addfb667efc93433a35ec43506b
Tai-Rocha <taina013@gmail.com>
2023-02-24 15:04:21 -0300

    updating assess_sp_uncertainty

--- R/assess_sp_uncertainty.R
+++ R/assess_sp_uncertainty.R
@@ -1,7 +1,14 @@
-#res_x, resolution of a grid system along horizontal axis
-#res_y, resolution of a grid system along vertical axis
-# this function assesses the spatial uncertainty of a grid system by calculating half of the diagonal of a grid cell
+#'  @title  Assesses the spatial uncertainty of a grid system by calculating half of the diagonal of a grid cell
+#'
+#' @param res_x Resolution of a grid system along horizontal axis
+#' @param res_y Resolution of a grid system along vertical axis
+#'
 assess_sp_uncertainty <-  function(res_x,res_y){
   output = (res_x^2+res_y^2)^0.5/2
   return (output)
 }
+
+
+#res_x, resolution of a grid system along horizontal axis
+#res_y, resolution of a grid system along vertical axis
+# t
